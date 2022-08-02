ebedf2ba06418b972110a14730e7bd20c03805ea
IsaacGH <garciaisaac468@gmail.com>
2022-08-02 15:57:52 -0500

    Intro slider is ready; Project's images have been resized

--- lib/init.dart
+++ lib/init.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
 
 class Init extends StatelessWidget {
   const Init({Key? key}) : super(key: key);
@@ -23,29 +24,54 @@ class Slider extends StatefulWidget {
 }
 
 class _SliderState extends State<Slider> {
+  List<Slide> slides = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    slides.add(
+      Slide(
+        pathImage: 'assets/images/meet.jpg',
+        description: '¡Descansa mientas esperas!',
+        styleDescription: const TextStyle(
+            color: Colors.black,
+            fontSize: 28
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+
+    slides.add(
+      Slide(
+        pathImage: 'assets/images/deliveryservice.jpg',
+        description: 'Llega al lugar donde estes',
+        styleDescription: const TextStyle(
+          color: Colors.black,
+          fontSize: 28
+        ),
+        backgroundColor: const Color.fromRGBO(232, 234, 246, 1),
+      ),
+    );
+  }
+
+  void onDonePress() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/meet.jpg',
-              width: MediaQuery.of(context).size.width - 40,
-            ),
-            const Text(
-              '¡Descansa mientras esperas!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'WorkSans',
-                fontSize: 28,
-                fontWeight: FontWeight.w400
-              ),
-            )
-          ],
-        ),
+    return IntroSlider(
+      slides: slides,
+      onDonePress: onDonePress,
+      showSkipBtn: false,
+      showPrevBtn: false,
+      showNextBtn: false,
+      showDoneBtn: true,
+      autoScroll: true,
+      autoScrollInterval: const Duration(seconds: 3),
+      doneButtonStyle: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(117, 72, 146, 1))
       ),
     );
   }
